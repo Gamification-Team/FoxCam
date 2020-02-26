47856940bc61fbf2994244f31a366b6851c33a99
Danielle Anderson <danielle.anderson2@marist.edu>
2020-02-26 12:20:23 -0500

    Button appears when menu button is clicked

---
+++ [ Foxcam ]/Foxcam/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+	//Buttons in Unity work by calling specified methods on Click
+	//To map a method to a specific button, go to the Inspector
+	//There is a Component called Button, inside is a little area
+	//called On Click; If its a new button this should say List is 
+	//Empty inside; near the bottom is a plus sign: click this and
+	//3 modules should appear. One of them says None. Click this and
+	//add the button you want to click on, once this is selected go
+	//and select the script you want to use (ButtonControl in this case)
+	//and choose the method you want
+
+	private bool isVisible;
+	private GameObject startSpot;
+	private GameObject button1;
+    private GameObject button2;
+    private GameObject button3;
+    private GameObject menuSpot;
+    private float xPos;
+    private float xStart;
+
+    void Start()
+    {
+    	isVisible = false;
+    	startSpot = GameObject.Find("startSpot");
+    	button1 = GameObject.Find("B1");
+    	button2 = GameObject.Find("B2");
+   		button3 = GameObject.Find("B3");
+    	menuSpot = GameObject.Find("MenuSpot");
+    	xPos = menuSpot.transform.position.x;
+    	xStart = startSpot.transform.position.x;
+    }
+
+    public void Slide()
+    {
+    	if (isVisible == false)
+    	{
+    		button1.transform.position = new Vector3(xPos, button1.transform.position.y, button1.transform.position.z);
+    		button2.transform.position = new Vector3(xPos, button2.transform.position.y, button2.transform.position.z);
+    		button3.transform.position = new Vector3(xPos, button3.transform.position.y, button3.transform.position.z);
+    		isVisible = true;
+    	}
+    	else
+    	{
+    		button1.transform.position = new Vector3(xStart, button1.transform.position.y, button1.transform.position.z);
+    		button2.transform.position = new Vector3(xStart, button2.transform.position.y, button2.transform.position.z);
+    		button3.transform.position = new Vector3(xStart, button3.transform.position.y, button3.transform.position.z);
+    		isVisible = false;
+    	}
+    }
+}
